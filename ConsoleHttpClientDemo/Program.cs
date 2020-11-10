a2cc639a9441cba66a1133fdbfa305a0d2ff7b56
ECR\Egon Rasmussen <ecr@live.dk>
2020-11-10 19:23:04 +0100

    ok

---
+++ ConsoleHttpClientDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleHttpClientDemo
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            SimpleRepository repos = new SimpleRepository();
+
+            Item newItem = new Item { Text = "New Item", Description = "My New Item" };
+            await repos.AddItem(newItem);
+
+
+            var items = await repos.GetItems();
+            foreach (Item item in items)
+            {
+                Console.WriteLine($"Text: {item.Text} - Description: {item.Description}");
+            }
+
+            Item singleItem = await repos.GetItemById(items[5].Id);
+            Console.WriteLine($"\nSingle Item: {singleItem.Text}");
+
+            Console.ReadLine();
+        }
+    }  
+}
---
+++ ConsoleHttpClientDemo/SimpleRepository.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHttpClientDemo
+{
+    public class SimpleRepository
+    {
+        private HttpClient _httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:5001/")
+        };
+
+        public async Task<List<Item>> GetItems()
+        {
+            string itemJson = await _httpClient.GetStringAsync($"api/item");
+            return JsonConvert.DeserializeObject<List<Item>>(itemJson);
+        }
+
+        public async Task<Item> GetItemById(string id)
+        {
+            var itemJson = await _httpClient.GetStringAsync($"api/item/{id}");
+            return JsonConvert.DeserializeObject<Item>(itemJson);
+        }
+
+        public async Task AddItem(Item item)
+        {
+            var itemJson = JsonConvert.SerializeObject(item);
+            var content = new StringContent(itemJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _httpClient.PostAsync("api/item", content);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong!");
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
